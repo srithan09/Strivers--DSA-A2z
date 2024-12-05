6d3f21585c27443a7de77e747c28e63241ad6278
Srithan M.S <123616762+srithan09@users.noreply.github.com>
2024-12-05 19:02:44 +0530

    commit-remove sony

--- Sony/43.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-void main()
-{
-char str[7] = "Citizen";
-printf ("%s", str);
-}
--- Sony/51.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main()
-{
-
-int i=5;
-
-printf("%d",i=++i ==6);
-
-}
