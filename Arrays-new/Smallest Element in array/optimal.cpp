cefbda6da75df43f79e760e324799541f8dfd014
Srithan M.S <123616762+srithan09@users.noreply.github.com>
2025-01-11 22:58:01 +0530

    arrays -new

---
+++ Arrays-new/Smallest Element in array/optimal.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int find_Largest(vector<int> arr)
+{
+    int max = INT_MAX;
+    for(int i=1;i<arr.size()-1;i++)
+    {
+        if(arr[i]<max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+//tc:O(N)
+int main()
+{
+    vector<int> arr;
+    arr ={100,20,789,988,34};
+    int ans = find_Largest(arr);
+    cout<<ans;
+}
