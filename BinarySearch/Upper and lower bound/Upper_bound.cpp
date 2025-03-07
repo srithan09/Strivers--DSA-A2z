916823e8acdcf5bbf399e635c5b8516aebd0e1c2
Srithan M.S <123616762+srithan09@users.noreply.github.com>
2025-03-07 07:21:53 +0000

    Added/Updated the document

---
+++ BinarySearch/Upper and lower bound/Upper_bound.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int upper_bound( vector<int> arr, int target)
+{
+    int start =0;
+    int end = arr.size()-1;
+    int ans = arr.size();
+    while(start<=end)
+    {
+        int mid = start + (end-start)/2;
+        if(arr[mid] >target)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid+1;
+        }
+    }
+    return ans;
+}
+int main() {
+    vector<int> arr = {1, 2, 4, 4, 5, 6, 8};
+    int target = 4 ;
+    
+    int result = upper_bound(arr, target);
+    cout << "The upper_bound index of " << target << " is: " << result << endl;
+
+    return 0;
+}
